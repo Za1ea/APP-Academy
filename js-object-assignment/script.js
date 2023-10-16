30ff6ef4db9cb57168c7833b8ee4fe0e119c028b
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-10-16 17:34:12 +0000

    finished problems 1,2 + half of 3

--- js-object-assignment/script.js
+++ js-object-assignment/script.js
@@ -2,35 +2,55 @@
 // Given an object of the stolen items for a household, return the total amount of items stolen (number). If nothing was robbed, return the string "Lucky you!”.
 
 function houseTotal(obj) {
+  let numStolenItems = 0;
+  let report;
 
+  for (let item in obj) {
+    numStolenItems += obj[item];
+  }
 
-return 
+  if (numStolenItems == 0) {
+    report = "Lucky you!";
+  }
+  else {
+    report = numStolenItems;
+  }
 
+  return report;
 }
 // uncomment these out to test it out
-//console.log(houseTotal(stolenItems[0])); 
-//console.log(houseTotal(stolenItems[1]));
-//console.log(houseTotal(stolenItems[2])); 
+console.log(houseTotal(stolenItems[0])); 
+console.log(houseTotal(stolenItems[1]));
+console.log(houseTotal(stolenItems[2])); 
  
 // PROBLEM TWO
 // Given an object of the stolen items for a household and a string in lowercase representing a specific item, return true if that item was stolen and false if it was not. 
 
 function isStolen(obj, item) {
-
-
+  if (obj[item] == 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
 
 }
 // uncomment these out to test it out
-//console.log(houseTotal(stolenItems[0], "keys")); 
-//console.log(houseTotal(stolenItems[1], "socks"));
-//console.log(houseTotal(stolenItems[2], "spoons")); 
+console.log(isStolen(stolenItems[0], "keys")); 
+console.log(isStolen(stolenItems[1], "socks"));
+console.log(isStolen(stolenItems[2], "spoons")); 
 
 
 // EXTRA CREDIT!!!!! PROBLEM THREE
 // Given an array of objects representing an entire neighborhood, return the most frequently stolen item. This is NOT the item that has been stolen from the most households, this is the item that has been stolen the most times TOTAL. 
 
 function neighborhoodTotal(arr) {
-
+  let stolenItemsArr = stolenItems[2];
+  for (let house in arr) {
+    for (let item in house) {
+      stolenItemsArr[item] = house[item];
+    }
+  }
 
   return total; 
   
