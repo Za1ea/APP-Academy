98941148e6cfd1b2be73360d80f3d71babc0baa7
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-10-18 16:51:13 +0000

    finished 3 and 4

--- js-object-assignment/script.js
+++ js-object-assignment/script.js
@@ -48,27 +48,30 @@ console.log(isStolen(stolenItems[2], "spoons"));
 // Given an array of objects representing an entire neighborhood, return the most frequently stolen item. This is NOT the item that has been stolen from the most households, this is the item that has been stolen the most times TOTAL. 
 
 function neighborhoodTotal(arr) {
-  let stolenItemsArr = Array(arr[0].length).fill(0);
+  let stolenItemsArr = Array(6).fill(0);
   let idx = 0;
-  console.log(arr);
-  for (let house in arr) {
+  // console.log(arr);
+  console.log(Object.keys(arr[0])[0]);
+  for (let i = 0; i < arr.length; i++) {
     // idx = 0;
-    console.log(house);
-    for (let i = 0; i < house.length; i++) {
-      let item = Object.keys(house)[i];
-      stolenItemsArr[i] += house[item];
-      // console.log(i);
+    let house = arr[i];
+    // console.log(house);
+    for (let j = 0; j < 6; j++) {
+      let item = Object.keys(house)[j];
+      stolenItemsArr[j] += house[item];
+      // console.log(item, house[item]);
+      // console.log(stolenItemsArr);
     }
   }
 
-  // console.log(stolenItemsArr);
+  console.log(stolenItemsArr);
 
   let maxIdx = arrMax(stolenItemsArr, idx = true);
   let key = Object.keys(arr[0])[maxIdx];
   let value = arrMax(stolenItemsArr, idx = false);
 
-  let total = key + maxIdx + value;
-  return value; 
+  let total = value + " " + key + " have been stolen!";
+  return total; 
   
 }
 
@@ -98,3 +101,5 @@ console.log(neighborhoodTotal(stolenItems));
 
 // PROBLEM FOUR
 // Based on the item that has been stolen the most, who do you think the burglar is? (you won’t get marked down for getting the “wrong” answer - just give me a fun explanation for why you think it is who it is! 
+
+// I think the cat is the burglar because cats like clothes and yarn and the most frequently stolen item was socks.
