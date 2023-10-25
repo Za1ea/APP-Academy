8b12c3ae90c0a18a9693d50f87567913263a2900
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-10-25 16:16:38 +0000

    edit

--- pagerank.js
+++ pagerank.js
@@ -12,16 +12,27 @@ function multiply(mat1, mat2) {
 }
 
 let changing = true;
-let L = [[0, 1/2, 1/2, 0, 0, 0, 1/2],[1/3,0,0,1/3,0,0,0], [1/3,0,0,1/3,0,0,0],[0,1/2,1/2,0,1/2,0,0], [0,0,0,1/3,0,1/2,0],[0,0,0,0,1/2,0,1/2], [1/3,0,0,0,0,1/2,0]];
+let L = [
+    [0  , 0.5, 0.5, 0  , 0  , 0  , 0.5],
+    [1/3, 0  , 0  , 1/3, 0  , 0  , 0  ],
+    [1/3, 0  , 0  , 1/3, 0  , 0  , 0  ],
+    [0  , 0.5, 0.5, 0  , 0.5, 0  , 0  ], 
+    [0  , 0  , 0  , 1/3, 0  , 0.5, 0  ],
+    [0  , 0  , 0  , 0  , 0.5, 0  , 0.5], 
+    [1/3, 0  , 0  , 0  , 0  , 0.5, 0  ]
+];
 let r = Array(7).fill(1/7);
 let res1 = multiply(L,r);
-let res2;
+let epoch = 0;
+// eigenvector = [3/2, 1, 1, 3/2, 1, 1, 1];
 
-// while (changing == true) {
-  // res2 = multiply(res1, r);
+while (epoch < 50) {
+  let res2 = multiply(L, res1);
   // if (res1 == res2) {
   //   changing = false;
   // }
-  // res1 = res2;
-// }
-alert(L);
+  res1 = res2;
+  console.log(res1);
+  epoch++;
+}
+// console.log(res1);
