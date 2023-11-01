0a3e3829171778698eabd6f5240f7250d74a3fee
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-11-01 17:07:49 +0000

    started sheetdb assignment

--- pagerank.js
+++ pagerank.js
@@ -1,4 +1,4 @@
-let N = 7;
+let N = 6;
 
 function multiply(mat1, mat2) { 
   let i, j, k; 
@@ -11,28 +11,20 @@ function multiply(mat1, mat2) {
   return res;
 }
 
-let changing = true;
 let L = [
-    [0  , 0.5, 0.5, 0  , 0  , 0  , 0.5],
-    [1/3, 0  , 0  , 1/3, 0  , 0  , 0  ],
-    [1/3, 0  , 0  , 1/3, 0  , 0  , 0  ],
-    [0  , 0.5, 0.5, 0  , 0.5, 0  , 0  ], 
-    [0  , 0  , 0  , 1/3, 0  , 0.5, 0  ],
-    [0  , 0  , 0  , 0  , 0.5, 0  , 0.5], 
-    [1/3, 0  , 0  , 0  , 0  , 0.5, 0  ]
+    [0  , 1/2, 0  , 1/3, 1/2, 1],
+    [1/4, 0  , 1/2, 0  , 0  , 0],
+    [0  , 1/2, 0  , 1/3, 0  , 0],
+    [1/4, 0  , 1/2, 0  , 1/2, 0], 
+    [1/4, 0  , 0  , 1/3, 0  , 0],
+    [1/4, 0  , 0  , 0  , 0  ,0], 
 ];
-let r = Array(7).fill(1/7);
-let res1 = multiply(L,r);
+let r = Array(N).fill(1/N);
 let epoch = 0;
-// eigenvector = [3/2, 1, 1, 3/2, 1, 1, 1];
 
-while (epoch < 50) {
-  let res2 = multiply(L, res1);
-  // if (res1 == res2) {
-  //   changing = false;
-  // }
-  res1 = res2;
-  console.log(res1);
+while (epoch < 70) {
+  let res = multiply(L, r);
+  r = res;
+  console.log(res);
   epoch++;
 }
-// console.log(res1);
