b3385eb9b40be082d1516c6af93c9cf1062f7c24
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-11-03 17:01:10 +0000

    added form and event listener

--- profile-page/script.js
+++ profile-page/script.js
@@ -1,39 +1,18 @@
-console.log("Hello 🌎");
 
-
-
-/**
-* Function to toggle bgColor
-* @param array colors Array of colors to set
-**/
-function flashColors(){
-  let ms = 500;
-  let colors = ['#92AFD7', '#5A7684', '#161B33'];
-  let counter = 0
-  let change = function(){
-        document.getElementByClassName("flash").style.backgroundColor =
-      colors[counter % colors.length]; // Change the color
-    counter++;
-     // Call the changer
-  };
-  change();
-  if (colors.length > 1) {setInterval(change, ms)}
-}
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'flash' class in style.css changes the appearance on click
-    btn.classList.toggle("flash");
-    if(btn.classList[0] = "flash") {
-      flashColors();
-    }
-  };
-}
+// Send data in comment form to sheetdb API
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+      response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    alert('success')
+  });
+});
 
 
 // ----- GLITCH STARTER PROJECT HELPER CODE -----
