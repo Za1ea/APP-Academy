916329bc5009c5a6205c9a0e4762bd86033122f1
Za1ea <108020818+Za1ea@users.noreply.github.com>
2023-11-07 18:08:35 +0000

    retrieves comments and posts them; need to fix css

--- profile-page/script.js
+++ profile-page/script.js
@@ -10,11 +10,46 @@ form.addEventListener("submit", e => {
       response => response.json()
   ).then((html) => {
     // you can put any JS code here
-    alert('success')
+    alert('success');
+    retrieveComments();
   });
 });
 
+window.onload = function() {
+  var reloading = sessionStorage.getItem("reloading");
+  if (reloading) {
+      sessionStorage.removeItem("reloading");
+      retrieveComments();
+  }
+}
 
+function retrieveComments() {
+  alert("retrieved");
+  fetch('https://sheetdb.io/api/v1/tyubq6ndvcsd5?sort_by=id')
+  .then((response) => response.json())
+  .then((data) => postComment(data));
+}
+
+function postComment(data) {
+  for (let i=0; i<data.length; i++) {
+    const commentBox = document.createElement("div");
+    const name = document.createElement("p");
+    const comment = document.createElement("p");
+
+    commentBox.appendChild(name);
+    commentBox.appendChild(comment);
+    commentBox.classList.add("comment");
+
+    name.innerHTML = data[i]["name"];
+    comment.innerHTML = data[i]["comment"]; 
+    name.classList.add("bold");
+
+    document.getElementById("comments").appendChild(commentBox); 
+  }
+  
+}
+
+retrieveComments();
 // ----- GLITCH STARTER PROJECT HELPER CODE -----
 
 // Open file when the link in the preview is clicked
