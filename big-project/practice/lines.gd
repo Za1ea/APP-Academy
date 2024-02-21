4bc3d435ff5cb30237d8d1cb3eb8f73e7f901c27
Za1ea <108020818+Za1ea@users.noreply.github.com>
2024-02-21 10:10:08 -0800

    updated lines

--- big-project/practice/lines.gd
+++ big-project/practice/lines.gd
@@ -1,15 +1,26 @@
-extends Node2D
+extends Control
 
+var show_circle = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_mouse_filter(Control.MOUSE_FILTER_PASS)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	'''if MOUSE_BUTTON_LEFT:
+		show_circle = not show_circle
+		print("circle")
+		queue_redraw()'''
+
+func _gui_input(event):
+	print("event")
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("I've been clicked D:")
 
 func _draw():
-	if (MOUSE_BUTTON_LEFT):
+	if (show_circle):
+		print("circle")
 		draw_circle(Vector2(75 / 2, 75 / 2), 15, Color.WHITE)
